b5e3430c1225f5c30f1d673b27ef7cfea9ed529e
xenedium <ahmed@abderraziq.com>
2023-07-02 18:21:44 +0100

    added c# lsp (Omnisharp)

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -5,6 +5,11 @@ local lspconfig = require "lspconfig"
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "pyright", "dockerls", "docker_compose_language_service" }
 
+lspconfig.omnisharp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "dotnet", "/home/xenedium/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" }
+}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
